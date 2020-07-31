235a633f0aa5b5dcd4669b7e18630ac78faceda2
Yurii Zubar <barsuk0831@gmail.com>
2020-07-31 10:34:07 +0300

    две задачи

--- taskjs/task.js
+++ taskjs/task.js
@@ -1,14 +1,77 @@
-const clients = ["Mango", "Ajax", "Poly", "Kiwi"];
+// task 1
 
-// Вернет новый массив в котором будут элементы с индексами от 1 до 2
-console.log(clients.slice(1, 3)); // ["Ajax", "Poly"]
+// const getItemsString = function (array) {
+//   // Write code under this line
+//   let result = "";
+//   for (let i = 0; i < array.length; i += 1) {
+//     result += `${i + 1} - ${array[i]}\n`;
+//   }
+//   return result;
+// };
 
-// Вернет новый массив в котором будут
-// элементы с индексами от 1 до (clients.length - 1)
-console.log(clients.slice(1)); // ["Ajax", "Poly", "Kiwi"]
+// console.log(getItemsString(["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"]));
+// /*
+// '1 - Mango
+// 2 - Poly
+// 3 - Ajax
+// 4 - Lux
+// 5 - Jay
+// 6 - Kong
+//  console.log(getItemsString([5, 10, 15]));
+/*
+'1 - 5
+2 - 10
+3 - 15
+'
+*/
 
-// Вернет копию исходного массива
-console.log(clients.slice()); // ["Mango", Ajax", "Poly", "Kiwi"]
+// task 2
 
-// Вернет новый массив состоящих из последних двух элементом исходного
-console.log(clients.slice(-2)); // ["Poly", "Kiwi"]
+// const calculateEngravingPrice = (message = "", pricePerWord = 0) =>
+//   // Write code in this line
+//   (pricePerWord = message.split(" ").length * pricePerWord);
+// console.log(
+//   calculateEngravingPrice(
+//     "Proin sociis natoque et magnis parturient montes mus",
+//     10
+//   )
+// ); // 80
+
+// console.log(
+//   calculateEngravingPrice(
+//     "Proin sociis natoque et magnis parturient montes mus",
+//     20
+//   )
+// ); // 160
+
+// console.log(
+//   calculateEngravingPrice("Donec orci lectus aliquam est magnis", 40)
+// ); // 240
+
+// console.log(
+//   calculateEngravingPrice("Donec orci lectus aliquam est magnis", 20)
+// ); // 120
+
+// console.log(calculateEngravingPrice("Uno", 100)); // 100
+
+// task 3
+
+function findLongestWord(string = "") {
+  // Write code under this line
+  const elementString = [];
+  const strings = string.split(" ");
+  for (const elementStrings of strings) {
+    console.log(elementStrings);
+  }
+  const elementStrings = elementString.split(" ");
+  return elementStrings;
+}
+
+console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
+// 'jumped'
+
+//console.log(findLongestWord('Google do a roll'));
+// 'Google'
+
+//console.log(findLongestWord('May the force be with you'));
+// 'force'
