c3905bff3afea11f2550695b92ef705f6492af2d
Yurii Zubar <barsuk0831@gmail.com>
2020-07-31 21:46:07 +0300

    task 7

--- taskjs/task.js
+++ taskjs/task.js
@@ -138,3 +138,33 @@
 
 // console.log(mapArray([-2.5, 0, 2.5]));
 // // [-25, 0, 25]
+
+// task 7
+
+// function filterArray(array) {
+//   const numbers = [];
+//   for (let i = 0; i < array.length; i += 1) {
+//     // Write code under this line
+//     if (Number.isFinite(array[i])) {
+//       numbers.push(array[i]);
+//     }
+//   }
+
+//   return numbers;
+// }
+
+// console.log(filterArray([-2, 0, 2]));
+// // [-2, 0, 2]
+
+// console.log(filterArray([1, NaN, Infinity]));
+// // [1]
+
+// console.log(filterArray([0, -0, 100, "100"]));
+// // [0, 0, 100]
+
+// console.log(filterArray([undefined, false, null, [], 1]));
+// // [1]
+
+// console.log(filterArray([{}, () => {}, 2]));
+// // [2]
+//  Number.isFinite
